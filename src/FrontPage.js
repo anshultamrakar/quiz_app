5ad5d8acb7b9e0af857184211aa47e0cd1bfd437
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-04-15 18:21:57 +0530

    added Mui

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -14,17 +14,41 @@ class FrontPage extends Component {
   render() { 
    
   return (
-    <div >
-       <AppBar position="static">
+    <div className='Body'>
+      <header className = "Header">
+       <AppBar position="static" >
             <Toolbar>
               <IconButton edge="start"  color="inherit" aria-label="menu">
               </IconButton>
-              <Typography variant="h6" >
+              <Typography variant="h4" >
                 Quiz-App
               </Typography>
             </Toolbar>
           </AppBar>
+    </header>
+    <main className='Main'>
+      <form handleSubmit = {(e) => e.preventDefault()}>
+        <p>
+        <label>Enter your name </label>
+        <input type = "text" placeholder='Enter your name'/>
+        </p>
+        <p>
+        <label>Enter your Gender </label>
+        <input type = "radio" placeholder='Enter your name'/>
+        <label>Male</label>
+        </p>
+        <p>
+        <label>Select your test </label>
+        <input type = "radio" placeholder='Enter your name'/>
+        <label>Male</label>
+        </p>
+        <button>Submit</button>
+      </form>
+    </main>
     </div>
+    
+   
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FrontPage from './FrontPage';
+import './index.css';
 
 
 
